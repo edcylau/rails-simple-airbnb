6b50906bff55915fb2e702d44c1f3b184937229b
Edmund Lau <tmlcyaa@gmail.com>
2020-01-08 15:59:14 +0800

    controller for flats completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "flats", to: "flats#index"
+  # get "flats/new", to: "flats#new"
+  # post "flats", to: "flats#create"
+  # get "flats/:id", to: "flats#show"
+  # get "flats/:id", to: "flats#edit"
+  # patch "flats/:id", to: "flats#update"
+  # delete "flats/:id", to: "flats#delete"
+  resources :flats
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+## Destroy
+
+puts "Destroy all flats"
+Flat.destroy_all
+
+flat_data = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3,
+    city: "London"
+  },
+  {
+    name: 'Great View Liverpool',
+    address: '20 Grosnenor Gardens Liverpool W7 12V',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 105,
+    number_of_guests: 2,
+    city: "Liverpool"
+  },
+  {
+    name: 'Perfect View Lancaster',
+    address: '30 Grosnenor Gardens Lancaster W7 12V',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 105,
+    number_of_guests: 2,
+    city: "Lancaster"
+  }
+]
+
+flat_data.each do |flat|
+  flat_created = Flat.create!(flat)
+  puts "#{flat_created.name} is created"
+end
